4c51ccb3a95da3e2ad706d4137d292062c557e26
mere <git@zoltanb.co.uk>
2014-09-28 17:41:34 +0100

    added github links

--- js/shared/Footer.jsx
+++ js/shared/Footer.jsx
@@ -8,6 +8,8 @@ module.exports = React.createClass({
         <div className="pure-menu pure-menu-horizontal pure-menu-open">
             <ul>
                 <li><a href="/about">About</a></li>
+                <li><a href="https://github.com/mere/iphone-store-checker/issues" target="_blank">Issues</a></li>
+                <li><a href="https://github.com/mere/iphone-store-checker#iphone-store-checker" target="_blank">GitHub</a></li>
                 
             </ul>
         </div>
