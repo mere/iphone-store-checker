5b2b44011477d73de7d841c637d9e5341a2fd032
Ⓩⓞⓛⓣⓐⓝ ℬℴurnℯ <github@zoltanb.co.uk>
2014-10-06 17:14:04 +0100

    Update main.jsx

--- js/server/main.jsx
+++ js/server/main.jsx
@@ -28,35 +28,3 @@ main.render = function(model, cb){
 }
 
 module.exports = main
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function getSomeData(cb){
-  setTimeout(function(){
-    request('http://localhost:3000/api/Stores'
-    , function (error, res, body) {
-    if (!error && res.statusCode == 200) {
-      cb(JSON.parse(body))
-    }
-    else {
-      console.log("Stores is being flaky...retrying...")
-      setTimeout(function(){getSomeData(cb)}, 2000)
-    }
-  })  
-  }, Math.random()*200)
-  
-}
